4ce5de5bf54c2dabce756aa6d0195d0449e08a71
Cole Perschon <coleperschon@gmail.com>
2023-12-08 21:44:45 -0800

    Prep day 10

---
+++ day10/part1.py
@@ -0,0 +1,14 @@
+import sys
+
+# Append utils path
+sys.path.append('/Users/cperschon/sources-external/advent-of-code-2023/')
+
+from utils import *
+
+filename = sys.argv[1]
+
+# ---------------------------------------- HELPERS ----------------------------------------
+
+# -----------------------------------------------------------------------------------------
+
+print(filename)
